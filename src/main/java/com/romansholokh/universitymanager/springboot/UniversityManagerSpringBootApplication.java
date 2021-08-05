3bb03de0aeb34eff12486bc72bbeebbd368f0b30
Roman <sholokh.r@gmail.com>
2021-08-05 16:47:11 +0300

    Initial commit

---
+++ src/main/java/com/romansholokh/universitymanager/springboot/UniversityManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.romansholokh.universitymanager.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityManagerSpringBootApplication.class, args);
+    }
+
+}
